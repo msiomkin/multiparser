d05a46d010b5a08b6098561bd62a9f9ce4325c42
Mike Siomkin <msiomkin@mail.ru>
2021-06-01 12:45:09 +0300

    Fix the wrap

--- multiparser.lua
+++ multiparser.lua
@@ -36,69 +36,6 @@ void * multipart_parser_get_data(multipart_parser* p);
 local package = package.search("libmultipart")
 local parserLib = ffi.load(package)
 
-local function setHeaderNameHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setHeaderNameHandler(...) instead of parser.setHeaderNameHandler(...)")
-
-    self.callbacks.on_header_field = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
-local function setHeaderValueHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setHeaderValueHandler(...) instead of parser.setHeaderValueHandler(...)")
-
-    self.callbacks.on_header_value = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
-local function setHeadersEndHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setHeadersEndHandler(...) instead of parser.setHeadersEndHandler(...)")
-
-    self.callbacks.on_headers_complete = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
-local function setPartDataBeginHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setPartDataBeginHandler(...) instead of parser.setPartDataBeginHandler(...)")
-
-    self.callbacks.on_part_data_begin = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
-local function setPartDataHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setPartDataHandler(...) instead of parser.setPartDataHandler(...)")
-
-    self.callbacks.on_part_data = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
-local function setPartDataEndHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setPartDataEndHandler(...) instead of parser.setPartDataEndHandler(...)")
-
-    self.callbacks.on_part_data_end = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
-local function setBodyEndHandler(self, handler, ...)
-    assert(self ~= nil,
-        "Use parser:setBodyEndHandler(...) instead of parser.setBodyEndHandler(...)")
-
-    self.callbacks.on_body_end = ffi.cast("multipart_data_cb", function(_, buf, length)
-        handler(self, buf, length, ...)
-    end)
-end
-
 local function onHeaderName(self, buf, length)
     local name = ffi.string(buf, length)
     assert(name ~= nil, "A part's header name is empty")
@@ -117,163 +54,346 @@ local function onHeaderValue(self, buf, length)
     return 0
 end
 
-local function onPartDataBegin(self, buf, length, dir, mode, fileNameHandler)
+local function getHeaderField(header, fieldName)
+    assert(header ~= nil, "'header' is nil")
+    assert(fieldName ~= nil and fieldName ~= "", "'fieldName' is not specified")
+
+    return string.match(header, fieldName .. '="?([^"^;]*)"?')
+end
+
+local finalizeFile
+
+local function onPartData(self, buf, length)
+    if self.key ~= nil then
+        assert(self.value ~= nil, "self.value is not initialized")
+        local value = ffi.string(buf, length)
+        self.value = self.value .. value
+
+        return 0
+    end
+
+    if self.file == nil then
+        local contentDisposition = self.headers["content-disposition"]
+        if contentDisposition == nil then
+            error("A part's 'Content-Disposition' header is not found")
+        end
+
+        local fileName = getHeaderField(contentDisposition, "filename")
+        if fileName == nil then
+            local key = getHeaderField(contentDisposition, "name")
+            if key == nil then
+                error("A part's name is not specified")
+            end
+
+            self.key = key
+
+            local value = ffi.string(buf, length)
+            self.value = value
+
+            return 0
+        end
+
+        if self.onFileName ~= nil then
+            fileName = self:onFileName(fileName)
+        end
+
+        self.fileName = fileName
+
+        if self.file ~= nil then
+            finalizeFile(self)
+        end
+
+        local fullTempFileName
+
+        local tmpDir = self.cfg.tempDirectory
+        if tmpDir ~= nil then
+            assert(tmpDir ~= (self.cfg.dir or ""),
+                "Temporary and main files directory coincide")
+            local dirMode = self.cfg.dirMode or 755
+
+            local ok, err = fio.mktree(tmpDir, tonumber(dirMode, 8))
+            if not ok then
+                error(err)
+            end
+
+            fullTempFileName = fio.pathjoin(tmpDir, fileName)
+        else
+            fullTempFileName = os.tmpname()
+        end
+
+        self.fullTempFileName = fullTempFileName
+
+        local fileMode = self.cfg.fileMode or 644
+        local file, err = fio.open(fullTempFileName, { "O_CREAT", "O_WRONLY" },
+            tonumber(fileMode, 8))
+        if file == nil then
+            error(err)
+        end
+
+        self.file = file
+    end
+
     local data = ffi.string(buf, length)
 
-    local fullFileName
-    local contentDisposition = self.headers["content-disposition"]
-    if contentDisposition == nil then
-        error("A part's 'Content-Disposition' header is not found")
-    end
+    self.file:write(data)
 
-    local fileName = string.match(contentDisposition, 'filename="([^"].-)"')
-    if fileName == nil then
-        error("A part's file name is not specified")
-    end
+    return 0
+end
 
-    if fileNameHandler ~= nil then
-        fileName = fileNameHandler(fileName)
-    end
-
-    fullFileName = fio.pathjoin(dir, fileName)
-
+local function onPartEnd(self)
     if self.file ~= nil then
         self.file:close()
         self.file = nil
+
+        local fullTempFileName = self.fullTempFileName
+        assert(fullTempFileName ~= nil, "self.fullTempFileName is nil")
+
+        local dir = self.cfg.directory or ""
+        local dirMode = self.cfg.dirMode or 755
+
+        local ok, err = fio.mktree(dir, tonumber(dirMode, 8))
+        if not ok then
+            error(err)
+        end
+
+        local fileName = self.fileName
+        assert(fileName ~= nil, "self.fileName is nil")
+
+        local fullFileName = fio.pathjoin(dir, fileName)
+
+        ok, err = fio.copyfile(fullTempFileName, fullFileName)
+        if not ok then
+            error(err)
+        end
+
+        ok, err = fio.unlink(fullTempFileName)
+        if not ok then
+            error(err)
+        end
+
+        if self.onFileProcessed ~= nil then
+            self:onFileProcessed(fileName)
+        end
+
+        self.fileName = nil
+        self.fullTempFileName = nil
+    else
+        assert(self.key ~= nil, "self.key is nil")
+        assert(self.value ~= nil, "self.value is nil")
+
+        self.values[self.key] = self.value
+
+        self.key = nil
+        self.value = nil
     end
-
-    mode = mode or 644
-
-    local file, err = fio.open(fullFileName, { "O_CREAT", "O_WRONLY" },
-        tonumber(mode, 8))
-    if file == nil then
-        error(err)
-    end
-
-    file:write(data)
-
-    return 0
-end
-
-local function onPartData(self, buf, length)
-    local data = ffi.string(buf, length)
-
-    assert(self.file ~= nil, "A part's file handle is not initialized")
-
-    self.file:write(data)
-
-    return 0
-end
-
-local function onPartDataEnd(self, buf, length)
-    local data = ffi.string(buf, length)
-
-    assert(self.file ~= nil, "A part's file handle is not initialized")
-
-    self.file:write(data)
 
     self.headers = { }
 
-    if self.file ~= nil then
-        self.file:close()
-        self.file = nil
-    end
-
     return 0
 end
 
-local function setDefaultHandlers(self, dir, mode, fileNameHandler)
-    assert(self ~= nil,
-        "Use parser:setDefaultHandlers(...) instead of parser.setDefaultHandlers(...)")
-
-    self:setHeaderNameHandler(onHeaderName)
-    self:setHeaderValueHandler(onHeaderValue)
-    self:setPartDataBeginHandler(onPartDataBegin, dir, mode, fileNameHandler)
-    self:setPartDataHandler(onPartData)
-    self:setPartDataEndHandler(onPartDataEnd)
+local function setReadHandler(self, onRead)
+    self.onRead = onRead
 end
 
-local function read(self, buf)
+local function setFileNameHandler(self, onFileName)
+    self.onFileName = onFileName
+end
+
+local freeWriteHandlers
+
+local function setWriteHandlers(self, handlers)
+    assert(self.parser == nil, "Can't change writing handlers when data processing is already in progress")
+
+    handlers = handlers or { }
+
+    freeWriteHandlers(self)
+
+    if handlers.onPartBegin ~= nil then
+        self.writeHandlers.on_part_data_begin = ffi.cast("multipart_notify_cb", function(_)
+            return handlers.onPartBegin(self)
+        end)
+    end
+
+    if handlers.onHeaderName ~= nil then
+        self.writeHandlers.on_header_field = ffi.cast("multipart_data_cb", function(_, buf, length)
+            return handlers.onHeaderName(self, buf, length)
+        end)
+    end
+
+    if handlers.onHeaderValue ~= nil then
+        self.writeHandlers.on_header_value = ffi.cast("multipart_data_cb", function(_, buf, length)
+            return handlers.onHeaderValue(self, buf, length)
+        end)
+    end
+
+    if handlers.onHeadersComplete ~= nil then
+        self.writeHandlers.on_headers_complete = ffi.cast("multipart_notify_cb", function(_)
+            return handlers.onHeadersComplete(self)
+        end)
+    end
+
+    if handlers.onPartData ~= nil then
+        self.writeHandlers.on_part_data = ffi.cast("multipart_data_cb", function(_, buf, length)
+            return handlers.onPartData(self, buf, length)
+        end)
+    end
+
+    if handlers.onPartEnd ~= nil then
+        self.writeHandlers.on_part_data_end = ffi.cast("multipart_notify_cb", function(_)
+            return handlers.onPartEnd(self)
+        end)
+    end
+
+    if handlers.onBodyEnd ~= nil then
+        self.writeHandlers.on_body_end = ffi.cast("multipart_notify_cb", function(_)
+            return handlers.onBodyEnd(self)
+        end)
+    end
+end
+
+local function setFileProcessedHandler(self, onFileProcessed)
+    self.onFileProcessed = onFileProcessed
+end
+
+local function step(self, buf)
     assert(self ~= nil,
         "Use parser:read(...) instead of parser.read(...)")
     assert(buf ~= nil, "'buf' is not set")
 
-    parserLib.multipart_parser_execute(self.parser, buf, #buf)
+    if self.parser == nil then
+        self.parser = parserLib.multipart_parser_init(self.boundary, self.writeHandlers)
+    end
+
+    return parserLib.multipart_parser_execute(self.parser, buf, #buf)
 end
 
-local function getVars(self)
-    assert(self ~= nil,
-        "Use parser:getVars() instead of parser.getVars()")
+local function run(self)
+    assert(self.onRead ~= nil,
+        "Data reading callback is not specified, call parser:setReadHandler(...)")
 
-    return self.vars
+    local bytesRead = 0
+
+    local ok, err = pcall(function()
+        while true do
+            local buf = self:onRead() or ""
+
+            local count = self:step(buf)
+            bytesRead = bytesRead + count
+
+            if buf == "" then
+                break
+            end
+        end
+    end)
+
+    self:free()
+
+    if not ok then
+        error(err)
+    end
+
+    return bytesRead
+end
+
+local function getValues(self)
+    assert(self ~= nil,
+        "Use parser:getValues() instead of parser.getValues()")
+
+    return self.values
+end
+
+finalizeFile = function(self)
+    if self.file ~= nil then
+        self.file:close()
+        self.file = nil
+        if self.fullTempFileName ~= nil then
+            fio.unlink(self.fullTempFileName)
+        end
+    end
+end
+
+freeWriteHandlers = function(self)
+    if self.writeHandlers.on_part_data_begin ~= nil then
+        self.writeHandlers.on_part_data_begin:free()
+        self.writeHandlers.on_part_data_begin = nil
+    end
+
+    if self.writeHandlers.on_header_field ~= nil then
+        self.writeHandlers.on_header_field:free()
+        self.writeHandlers.on_header_field = nil
+    end
+
+    if self.writeHandlers.on_header_value ~= nil then
+        self.writeHandlers.on_header_value:free()
+        self.writeHandlers.on_header_value = nil
+    end
+
+    if self.writeHandlers.on_headers_complete ~= nil then
+        self.writeHandlers.on_headers_complete:free()
+        self.writeHandlers.on_headers_complete = nil
+    end
+
+    if self.writeHandlers.on_part_data ~= nil then
+        self.writeHandlers.on_part_data:free()
+        self.writeHandlers.on_part_data = nil
+    end
+
+    if self.writeHandlers.on_part_data_end ~= nil then
+        self.writeHandlers.on_part_data_end:free()
+        self.writeHandlers.on_part_data_end = nil
+    end
+
+    if self.writeHandlers.on_body_end ~= nil then
+        self.writeHandlers.on_body_end:free()
+        self.writeHandlers.on_body_end = nil
+    end
 end
 
 local function free(self)
     assert(self ~= nil,
         "Use parser:free() instead of parser.free()")
 
-    if self.file ~= nil then
-        self.file:close()
-    end
+    finalizeFile(self)
 
-    if self.callbacks.on_header_field ~= nil then
-        self.callbacks.on_header_field:free()
-    end
+    freeWriteHandlers(self)
 
-    if self.callbacks.on_header_value ~= nil then
-        self.callbacks.on_header_value:free()
+    if self.parser ~= nil then
+        parserLib.multipart_parser_free(self.parser)
+        self.parser = nil
     end
-
-    if self.callbacks.on_headers_complete ~= nil then
-        self.callbacks.on_headers_complete:free()
-    end
-
-    if self.callbacks.on_part_data_begin ~= nil then
-        self.callbacks.on_part_data_begin:free()
-    end
-
-    if self.callbacks.on_part_data ~= nil then
-        self.callbacks.on_part_data:free()
-    end
-
-    if self.callbacks.on_part_data_end ~= nil then
-        self.callbacks.on_part_data_end:free()
-    end
-
-    if self.callbacks.on_body_end ~= nil then
-        self.callbacks.on_body_end:free()
-    end
-
-    parserLib.multipart_parser_free(self.parser)
 end
 
-local function new(boundary)
+local function new(boundary, cfg)
     assert(boundary ~= nil and boundary ~= "",
         "'boundary is not set'")
 
-    local callbacks = ffi.new("multipart_parser_settings")
-    local parser = parserLib.multipart_parser_init(boundary, callbacks)
+    local writeHandlers = ffi.new("multipart_parser_settings")
 
     local obj = {
-        boundary = boundary,
-        callbacks = callbacks,
-        parser = parser,
+        boundary = "--" .. boundary,
+        writeHandlers = writeHandlers,
+        cfg = cfg or { },
         headers = { },
-        vars = { }
+        values = { }
     }
+
+    setWriteHandlers(obj, {
+        onHeaderName = onHeaderName,
+        onHeaderValue = onHeaderValue,
+        onPartData = onPartData,
+        onPartEnd = onPartEnd
+    })
 
     return setmetatable(obj, {
         __index = {
-            setHeaderNameHandler = setHeaderNameHandler,
-            setHeaderValueHandler = setHeaderValueHandler,
-            setHeadersEndHandler = setHeadersEndHandler,
-            setPartDataBeginHandler = setPartDataBeginHandler,
-            setPartDataHandler = setPartDataHandler,
-            setPartDataEndHandler = setPartDataEndHandler,
-            setBodyEndHandler = setBodyEndHandler,
-            setDefaultHandlers = setDefaultHandlers,
-            read = read,
-            getVars = getVars,
+            setReadHandler = setReadHandler,
+            setFileNameHandler = setFileNameHandler,
+            setWriteHandlers = setWriteHandlers,
+            setFileProcessedHandler = setFileProcessedHandler,
+            step = step,
+            run = run,
+            getValues = getValues,
             free = free
         },
         __gc = function(self)
